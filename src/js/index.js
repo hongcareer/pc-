082bbf75aaf446670ac2d31c54ba8ae5ccb68817
hongcareer <hongcareer@126.com>
2018-11-23 19:35:14 +0800

    箭头，导航，页面的同步移动

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,65 @@
 import '../less/index.less';
 const liNodes = document.querySelectorAll('.header-main .nav li');
 const arrow = document.querySelector('.header-main .arrow');
+const ulList = document.querySelector('#content .list');
+const content = document.querySelector('#content');
+let nowIndex = 0;
 arrow.style.left = liNodes[0].getBoundingClientRect().left+(liNodes[0].offsetWidth/2-arrow.offsetWidth/2)+'px';
-
+//头部的逻辑
 for (let i = 0; i < liNodes.length; i++) {
   liNodes[i].onclick = function(){
-    for (var j = 0; j < liNodes.length; j++) {
-      liNodes[j].className = '';
-    };
-    liNodes[i].className = 'active';
-    arrow.style.left = liNodes[i].getBoundingClientRect().left+liNodes[i].offsetWidth/2-arrow.offsetWidth/2+'px';
-    console.log(arrow.offsetLeft);
+    nowIndex = i;
+    move(nowIndex);
   }
+}
+//内容区
+document.onmousewheel = wheel;
+document.addEventListener('DOMMouseScroll',wheel);
+function wheel(event) {
+  event = event || window.event;
+  let flag = '';
+  if (event.wheelDelta) {
+    //ie/chrome
+    if (event.wheelDelta > 0) {
+      flag = 'up';
+    } else {
+      flag = 'down'
+    }
+  } else if (event.detail) {
+    //firefox
+    if (event.detail < 0) {
+      flag = 'up';
+    } else {
+      flag = 'down'
+    }
+  }
+
+  switch (flag) {
+    case 'up' :
+      if(nowIndex>0){
+        nowIndex--;
+        move(nowIndex);
+      }
+
+      break;
+    case 'down' :
+      if(nowIndex<4){
+        nowIndex++;
+        move(nowIndex);
+      }
+      break;
+  }
+
+  //禁止默认行为
+  event.preventDefault && event.preventDefault();
+  return false;
+};
+//箭头，导航，页面的同步移动
+function move(nowIndex) {
+  ulList.style.top = -nowIndex*content.offsetHeight+'px';
+  for (var j = 0; j < liNodes.length; j++) {
+    liNodes[j].className = '';
+  };
+  liNodes[nowIndex].className = 'active';
+  arrow.style.left = liNodes[nowIndex].getBoundingClientRect().left+liNodes[nowIndex].offsetWidth/2-arrow.offsetWidth/2+'px';
 }

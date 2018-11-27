165656ab422b03bd5f95bff78f977b1a9f518e4e
hongcareer <hongcareer@126.com>
2018-11-27 08:28:19 +0800

    修改了一些bug

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,97 +1,104 @@
-// const {resolve} = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const webpack = require('webpack');
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const CleanCSSPlugin = require("less-plugin-clean-css");
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
-//
-// module.exports = {
-//   entry:['./src/js/index.js ','./src/index.html'],
-//   output: {
-//     path:resolve(__dirname,'../dist'),
-//     filename: './js/[hash:10].js'
-//   },
-//   module: {
-//     rules:[
-//       {
-//         test: /\.less$/,
-//         use: ExtractTextPlugin.extract({
-//           fallback: 'style-loader',
-//           //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
-//           use: ['css-loader', 'postcss-loader',{
-//             loader: "less-loader", options: {
-//               plugins: [
-//                 new CleanCSSPlugin({ advanced: true })//压缩css文件
-//               ]
-//             }
-//           }]
-//         })
-//       },
-//       {
-//         test: /\.(png|jpg|gif|mp3)$/,
-//         use: [
-//           {
-//             loader: 'url-loader',
-//             options: {
-//               limit: 8192,
-//               publicPath:'./images',
-//               outputPath:'images',
-//               name: '[name].[ext]'
-//             }
-//           }
-//         ]
-//       },
-//       {
-//         test: /\.(html)$/,
-//         use: {
-//           loader: 'html-loader',
-//         }
-//       },
-//       {
-//         test: /\.js$/, // 涵盖 .js 文件
-//         enforce: "pre", // 预先加载好 jshint loader
-//         exclude: /node_modules/, // 排除掉 node_modules 文件夹下的所有文件
-//         use: [
-//           {
-//             loader: "jshint-loader",
-//             options: {
-//               camelcase: true,
-//               emitErrors: false,
-//               failOnHint: false,
-//               reporter: function(errors) { }
-//             }
-//           }
-//         ]
-//       },
-//       {
-//         test: /\.js$/,
-//         exclude: /(node_modules|bower_components)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env']
-//           }
-//         }
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html',
-//       minify: {    //压缩html代码
-//         removeComments: true,
-//         collapseWhitespace: true
-//       }
-//
-//     }),
-//     new ExtractTextPlugin("./[hash:10].css"),//提取css文件
-//
-//     new webpack.optimize.UglifyJsPlugin(),//压缩js文件
-//     new CleanWebpackPlugin('./dist',{
-//       root: resolve(__dirname, '../'),
-//     })
-//   ]
-// }
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanCSSPlugin = require("less-plugin-clean-css");
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry:['./src/js/index.js ','./src/index.html'],
+  output: {
+    path:resolve(__dirname,'../dist'),
+    filename: './js/[hash:10].js'
+  },
+  module: {
+    rules:[
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
+          use: ['css-loader', 'postcss-loader',{
+            loader: "less-loader", options: {
+              plugins: [
+                new CleanCSSPlugin({ advanced: true })//压缩css文件
+              ]
+            }
+          }]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              publicPath:'./images',
+              outputPath:'images',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
+      {
+        test: /\.js$/, // 涵盖 .js 文件
+        enforce: "pre", // 预先加载好 jshint loader
+        exclude: /node_modules/, // 排除掉 node_modules 文件夹下的所有文件
+        use: [
+          {
+            loader: "jshint-loader",
+            options: {
+              camelcase: true,
+              //jslint 的错误信息在默认情况下会显示为 warning（警告）类信息
+              //将 emitErrors 参数设置为 true 可使错误显示为 error（错误）类信息
+              emitErrors: false,
+              //jshint 默认情况下不会打断webpack编译
+              //如果你想在 jshint 出现错误时，立刻停止编译
+              //请设置 failOnHint 参数为true
+              failOnHint: false,
+              esversion: 6,
+              loopfunc: true,
+              reporter: function(errors) { }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {    //压缩html代码
+        removeComments: true,
+        collapseWhitespace: true
+      }
+
+    }),
+    new ExtractTextPlugin("./[hash:10].css"),//提取css文件
+
+    new webpack.optimize.UglifyJsPlugin(),//压缩js文件
+    new CleanWebpackPlugin('./dist',{
+      root: resolve(__dirname, '../'),
+    })
+  ]
+}
 
 /*
   webpack的默认配置文件
@@ -117,12 +124,12 @@
 
    如何运行：在项目的根目录运行 webpack （默认会找配置文件）
  */
-const {resolve} = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CleanCSSPlugin = require("less-plugin-clean-css");
-const webpack = require('webpack');
+// const {resolve} = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const CleanCSSPlugin = require("less-plugin-clean-css");
+// const webpack = require('webpack');
 
 module.exports = {
   //入口起点
